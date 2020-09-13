6fb47f7e6b947331b30fdd4ec1e6d08bc1049db4
ymikaeru <ymikaeru@gmail.com>
2020-09-14 01:04:34 +0900

    change dist name to docs

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('useref', function() {
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('docs'));
 });
 
 // Optimizing Images 
@@ -63,24 +63,24 @@ gulp.task('images', function() {
     .pipe(cache(imagemin({
       interlaced: true,
     })))
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('docs/images'))
 });
 
 // Copying fonts 
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
+    .pipe(gulp.dest('docs/fonts'))
 })
 
 // Cleaning 
 gulp.task('clean', function() {
-  return del.sync('dist').then(function(cb) {
+  return del.sync('docs').then(function(cb) {
     return cache.clearAll(cb);
   });
 })
 
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+gulp.task('clean:docs', function() {
+  return del.sync(['docs/**/*', '!docs/images', '!docs/images/**/*']);
 });
 
 // Build Sequences
@@ -94,7 +94,7 @@ gulp.task('default', function(callback) {
 
 gulp.task('build', function(callback) {
   runSequence(
-    'clean:dist',
+    'clean:docs',
     'sass',
     ['useref', 'images', 'fonts'],
     callback
